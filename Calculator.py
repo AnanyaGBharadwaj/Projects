28462b64ddf148c2785e8156db2b618b9953b2b0
Ananya G Bharadwaj <ananya.gbharadwaj@gmail.com>
2025-06-30 22:15:04 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,6 +1,6 @@
 a=int(input("Enter the first number"))
 b=int(input("Enter the second number"))
-c=input("Choose the operation")
+c=input("Choose any operation")
 if c=='+':
     c=a+b
 elif c=='-':
