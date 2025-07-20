d34a393d6027f11a01c3fe02eb104157bb0317f7
Ananya G Bharadwaj <ananya.gbharadwaj@gmail.com>
2025-07-20 19:19:29 +0530

    Add files via upload

---
+++ swg.py
@@ -0,0 +1,21 @@
+import random
+for i in range(1,5):
+  x=random.randint(1,3)
+  a=int(input("Enter numbers from 1 to 3\n"))
+  print("Computer chose",x)
+  if a==x:
+    print("Draw")
+  elif a==1 and x==2:
+    print("You win")
+  elif a==1 and x==3:
+    print("AI wins")
+  elif a==2 and x==1:
+    print("You win")
+  elif a==2 and x==3:
+    print("AI wins")
+  elif a==3 and x==1:
+    print("AI wins")
+  else:
+    print("You win")
+    i=i+1
+    
